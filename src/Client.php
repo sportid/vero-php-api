4006a02677a7a3104231506bed8a695f4aef3d2a
Artur Käpp <arturkp@gmail.com>
2016-03-14 23:03:12 +0200

    Vero resubscribe

--- src/Client.php
+++ src/Client.php
@@ -74,6 +74,17 @@
       return $this->_send($endpoint, $request_data);
     }
 
+    public function resubscribe($user_id) {
+      $endpoint = "https://api.getvero.com/api/v2/users/resubscribe.json";
+      $request_data = array(
+        'auth_token'        => $this->auth_token,
+        'id'                => $user_id,
+        'development_mode'  => $this->development_mode
+      );
+
+      return $this->_send($endpoint, $request_data);
+    }
+
     public function track($event_name, $identity, $data, $extras = array()) {
       $endpoint = "https://api.getvero.com/api/v2/events/track.json";
       $request_data = array(
